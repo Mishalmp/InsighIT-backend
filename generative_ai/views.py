e73b3e49fa610d18a3c273b7ce2808e5d967c26d
Mishal Mp <mishalmp916@gmail.com>
2024-01-01 13:36:16 +0530

    chatgpt

--- generative_ai/views.py
+++ generative_ai/views.py
@@ -12,7 +12,6 @@ from decouple import config
 
 
 openAI_client = OpenAI(api_key=config('CHATGPT_API_KEY'))
-print(config('CHATGPT_API_KEY'),'apiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii')
 class ArticleContentcreationByOpenai(APIView):
     
     permission_classes = (IsAuthenticated,)
