554ee32c5290f88db7b7f1aa9c4dd92608d4753a
Mishal Mp <mishalmp916@gmail.com>
2024-01-04 14:52:49 +0530

    application asgi

--- insight/asgi.py
+++ insight/asgi.py
@@ -39,15 +39,25 @@ from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 from chats.routing import websocket_urlpatterns
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'insight.settings')
 
+# Get the Django ASGI application
+django_asgi_application = get_asgi_application()
+
+# WebSocket application
+websocket_application = ProtocolTypeRouter(
+    {
+        "websocket": AuthMiddlewareStack(
+            URLRouter(websocket_urlpatterns)
+        ),
+    }
+)
+
+# Combined application for both HTTP and WebSocket
 application = ProtocolTypeRouter(
     {
-        'http': get_asgi_application(),
-          "websocket": AuthMiddlewareStack(
-        URLRouter(
-            websocket_urlpatterns
-        )
-    ),
+        "http": django_asgi_application,
+        "websocket": websocket_application,
     }
 )
