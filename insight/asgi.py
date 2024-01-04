0c918f892d849b51278695143a0f11269db0ded3
Mishal Mp <mishalmp916@gmail.com>
2024-01-04 14:33:35 +0530

    application asgi

--- insight/asgi.py
+++ insight/asgi.py
@@ -35,19 +35,14 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 """
 
 import os
-
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
-
-
+from chats.routing import websocket_urlpatterns
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'insight.settings')
 
-def application(scope, receive, send):
-    from chats.routing import websocket_urlpatterns
-    # Any Django-related imports should be inside this function
-    return ProtocolTypeRouter(
-        {
-            'http': get_asgi_application(),
-            'websocket': URLRouter(websocket_urlpatterns),
-        }
-    )(scope, receive, send)
+application = ProtocolTypeRouter(
+    {
+        'http': get_asgi_application(),
+        'websocket': URLRouter(websocket_urlpatterns),
+    }
+)
